5f98ea8c7d5a330e9bee91d19659b3663f7c6455
sobiamustafa95 <sobiamustafa95@gmail.com>
2021-07-29 19:27:40 +0500

    App.js is updated

--- src/App.js
+++ src/App.js
@@ -5,29 +5,20 @@ import "./App.css";
 
 const App = () => {
 
+  function nCards(val) {
+    return (
+      <Card 
+        imgsrc = { val.imgsrc }
+        title = { val.title }
+        sname = { val.sname }
+        link = { val.links }
+      />
+    );
+  }
   return(
     <>
       <h1 className = "heading-style"> List of top 3 Netfllix Series </h1>
-      <Card 
-        imgsrc = { CardsData[0].imgsrc }
-        title = { CardsData[0].title }
-        sname = { CardsData[0].sname }
-        link = { CardsData[0].links }
-      />
-      <Card 
-        className = ""
-        imgsrc = { CardsData[1].imgsrc }
-        title = { CardsData[1].title }
-        sname = { CardsData[1].sname }
-        link = { CardsData[1].links }
-      />
-      <Card 
-        className = ""
-        imgsrc = { CardsData[2].imgsrc }
-        title = { CardsData[2].title }
-        sname = { CardsData[2].sname }
-        link = { CardsData[2].links }
-      />
+      { CardsData.map(nCards) }
     </>
   )
 }
